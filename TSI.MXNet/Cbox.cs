9c74afea9b5bc595c7a8f251e18b9e18a724e1b0
Jeff Pride <jpride@gmail.com>
2025-10-31 16:37:15 -0500

    PuSub Refinements

--- TSI.MXNet/Cbox.cs
+++ TSI.MXNet/Cbox.cs
@@ -36,6 +36,7 @@ namespace TSI.MXNet
         public event EventHandler<SimpleResponseEventArgs> SimpleResponseEvent;
         public event EventHandler<RouteEventArgs> RouteEvent;
         public event EventHandler<ConnectionStatusEventArgs> ConnectionStatusEvent;
+        public event EventHandler InitializationCompleteEvent;
 
         public string IPAddress
         {
@@ -65,14 +66,14 @@ namespace TSI.MXNet
             if (_instance == null)
             {                 
                 _instance = this;
-                CrestronConsole.PrintLine($"CBox Singleton Instance has been set");
+                //CrestronConsole.PrintLine($"CBox Singleton Instance has been set");
             }
             else
             {
                 throw new Exception("CBox is a singleton class and has already been instantiated.");
             }
 
-            CrestronConsole.PrintLine($"CBox Constructor");
+            //CrestronConsole.PrintLine($"CBox Constructor");
             mxnetDecoders = new List<MxnetDecoder>();
             mxnetEncoders = new List<MxnetEncoder>();
         }
@@ -85,6 +86,8 @@ namespace TSI.MXNet
                 _asyncClient.ResponseReceived += Client_ResponseReceived;
                 _asyncClient.ConnectionStatusChanged += Client_ConnectionChange;
                 _asyncClient.Initialize();
+
+                InitializationCompleteEvent?.Invoke(this, new EventArgs());
             }
             catch (Exception ex)
             {
@@ -95,7 +98,7 @@ namespace TSI.MXNet
 
         private void Client_ConnectionChange(object sender, bool e)
         {
-            DebugUtility.DebugPrint(_debug, $"Connection Status: " + e);
+            //DebugUtility.DebugPrint(_debug, $"Connection Status: " + e);
             
             ConnectionStatusEventArgs args = new ConnectionStatusEventArgs
             {
@@ -107,7 +110,7 @@ namespace TSI.MXNet
 
         private void Client_ResponseReceived(object sender, string response)
         {
-            DebugUtility.DebugPrint(_debug, $"Received: " + response);
+            //DebugUtility.DebugPrint(_debug, $"Received: " + response);
             SplitResponse(response);
         }
 
@@ -153,61 +156,53 @@ namespace TSI.MXNet
                 {
                     if (detailedResponse.Info != null && detailedResponse.Info.Any())
                     {
-                        CrestronConsole.PrintLine($"CBox: Parsing DeviceListResponse");
                         DeviceListUpdateEventArgs args = new DeviceListUpdateEventArgs();                        
 
-                        lock (_listLock)
+
+                        mxnetDecoders.Clear();
+                        mxnetEncoders.Clear();
+
+                        foreach (var kvp in detailedResponse.Info)
                         {
-                            mxnetDecoders.Clear();
-                            mxnetEncoders.Clear();
+                            string deviceId = kvp.Key;
+                            Device device = kvp.Value;
 
-                            foreach (var kvp in detailedResponse.Info)
+                            if (device.Modelname == "AC-MXNET-1G-R" | device.Modelname == "AC-MXNET-1G-D")
                             {
-                                string deviceId = kvp.Key;
-                                Device device = kvp.Value;
-
-                                if (device.Modelname == "AC-MXNET-1G-R" | device.Modelname == "AC-MXNET-1G-D")
+                                MxnetDecoder d = new MxnetDecoder
                                 {
-                                    MxnetDecoder d = new MxnetDecoder
-                                    {
-                                        id = device.Id,
-                                        ip = device.Ip,
-                                        mac = device.Mac,
-                                        modelname = device.Modelname,
-                                    };
-                                    CrestronConsole.PrintLine($"adding Decoder: {d.id} to 'mxnetDecoders' list");
-                                    mxnetDecoders.Add(d);
-                                }
-                                else if (device.Modelname == "AC-MXNET-1G-T")
-                                {
-                                    MxnetEncoder e = new MxnetEncoder
-                                    {
-                                        id = device.Id,
-                                        ip = device.Ip,
-                                        mac = device.Mac,
-                                        modelname = device.Modelname
-                                    };
-                                    CrestronConsole.PrintLine($"adding Encoder: {e.id} to 'mxnetEncoders' list");
-                                    mxnetEncoders.Add(e);
-                                }
+                                    id = device.Id,
+                                    ip = device.Ip,
+                                    mac = device.Mac,
+                                    modelname = device.Modelname,
+                                };
+                                mxnetDecoders.Add(d);
                             }
-
-                            mxnetDecoders = mxnetDecoders.OrderBy(d => d.id).ToList(); //Devices must be named with "01Decoder, 02 Decoder..."
-                            mxnetEncoders = mxnetEncoders.OrderBy(d => d.id).ToList();
-
+                            else if (device.Modelname == "AC-MXNET-1G-T")
+                            {
+                                MxnetEncoder e = new MxnetEncoder
+                                {
+                                    id = device.Id,
+                                    ip = device.Ip,
+                                    mac = device.Mac,
+                                    modelname = device.Modelname
+                                };
+                                mxnetEncoders.Add(e);
+                            }
                         }
 
+                        mxnetDecoders = mxnetDecoders.OrderBy(d => d.id).ToList(); //Devices must be named with "01Decoder, 02 Decoder..."
+                        mxnetEncoders = mxnetEncoders.OrderBy(d => d.id).ToList();
+                     
                         List<string> _encIdStrings = new List<string>();
                         foreach (MxnetEncoder e in mxnetEncoders)
                         {
-                            CrestronConsole.PrintLine($"adding Encoder: {e.id} @ {mxnetEncoders.FindIndex(x => x.id == e.id)}");
                             _encIdStrings.Add(e.id);
                         }
 
                         List<string> _decIdStrings = new List<string>();
                         foreach (MxnetDecoder d in mxnetDecoders)
-                        {
-                            CrestronConsole.PrintLine($"adding Decoder: {d.id} @ {mxnetDecoders.FindIndex(x => x.id == d.id)}");
+                        {   
                             _decIdStrings.Add(d.id);
                         }
 
@@ -232,7 +227,7 @@ namespace TSI.MXNet
 
                     SimpleResponseEvent?.Invoke(this, args);
 
-                    DebugUtility.DebugPrint(_debug, $"SimpleResponse Cmd: {simpleResponse.Cmd}");
+                    //DebugUtility.DebugPrint(_debug, $"SimpleResponse Cmd: {simpleResponse.Cmd}");
 
                     if ((simpleResponse.Cmd.Contains("matrix aset") || simpleResponse.Cmd.Contains("config set device videopathdisable")))
                     {
@@ -296,13 +291,12 @@ namespace TSI.MXNet
                     int decIndex;
                     int encIndex;
 
-                    lock (_listLock)
-                    {
-                        decIndex = mxnetDecoders.FindIndex(x => x.id == dec);
-                        encIndex = mxnetEncoders.FindIndex(x => x.id == enc);
-                    }
 
-                    DebugUtility.DebugPrint(_debug, $"ParseRouteRepsonse - decIndex: {decIndex} | encIndex: {encIndex}\n");
+                    decIndex = mxnetDecoders.FindIndex(x => x.id == dec);
+                    encIndex = mxnetEncoders.FindIndex(x => x.id == enc);
+                    
+
+                    //DebugUtility.DebugPrint(_debug, $"ParseRouteRepsonse - decIndex: {decIndex} | encIndex: {encIndex}\n");
 
                     if (decIndex != -1 && encIndex != -1)
                     {
@@ -310,15 +304,16 @@ namespace TSI.MXNet
 
                         RouteEventArgs args = new RouteEventArgs
                         {
+                            DecoderId = mxnetDecoders[decIndex].id,
                             DestIndex = (ushort)(decIndex),
                             SourceIndex = (ushort)(encIndex),
                             StreamOn = 1,
                             SourceId = mxnetEncoders[encIndex].id
                         };
 
-                        DebugUtility.DebugPrint(_debug, $"Sending Route Event to SImpl+\n");
-                        DebugUtility.DebugPrint(_debug, $"Dest[{args.DestIndex}] ==> {args.SourceIndex}\n");
-                        DebugUtility.DebugPrint(_debug, $"StreamOn: {args.StreamOn == 1} | StreamSource: {args.SourceId}");
+                        //DebugUtility.DebugPrint(_debug, $"Raising Route Event\n");
+                        //DebugUtility.DebugPrint(_debug, $"Dest[{args.DestIndex}] ==> {args.SourceIndex}\n");
+                        //DebugUtility.DebugPrint(_debug, $"StreamOn: {args.StreamOn == 1} | StreamSource: {args.SourceId}");
 
                         RouteEvent?.Invoke(this, args);
                     }
@@ -410,7 +405,7 @@ namespace TSI.MXNet
         }
         public void Switch(string type, ushort sourceIndex, ushort destIndex) //zero based
         {
-            DebugUtility.DebugPrint(_debug, $"SourceIndex: {sourceIndex} | DestIndex: {destIndex}");
+            //DebugUtility.DebugPrint(_debug, $"SourceIndex: {sourceIndex} | DestIndex: {destIndex}");
 
             try
             {
--- TSI.MXNet/MxnetDecoderClass.cs
+++ TSI.MXNet/MxnetDecoderClass.cs
@@ -5,18 +5,12 @@ using Crestron.SimplSharp;
 
 namespace TSI.MXNet
 {
-    /// <summary>
-    /// Manages a single Decoder.
-    /// This class is instantiated by Simpl+ and acts as the "translator"
-    /// between the CBox singleton and the S+ wrapper.
-    /// </summary>
     public class MxnetDecoderClass
     {
-        // --- S+ Feedback Events (S+ Compatible) ---
         public event EventHandler<RouteEventArgs> CurrentRouteChanged;
         public event EventHandler<ResponseErrorEventArgs> ErrorReceived;
 
-        // --- S+ Getter Properties (S+ Compatible) ---
+
         public ushort CurrentSourceIndex { get; private set; }
         public string CurrentSourceId { get; private set; }
         public ushort IsStreamOn { get; private set; }
@@ -24,61 +18,52 @@ namespace TSI.MXNet
         public string LastError { get; private set; }
         public string LastErrorCmd { get; private set; }
 
-        // --- Internal Properties ---
-        private string _myDecoderId; // The ID of this specific decoder
+        
+        private string _myDecoderId; 
 
 
         public MxnetDecoderClass()
         {
 
         }
-        /// <summary>
-        /// S+ must call this immediately after creation.
-        /// </summary>
+
         public void Initialize(string decoderId)
         {
             _myDecoderId = decoderId;
 
-            // Subscribe to the CBox singleton's C#-to-C# events
             CBox.Instance.RouteEvent += CBox_RouteEvent;
             CBox.Instance.ResponseErrorEvent += CBox_ResponseErrorEvent;
             CBox.Instance.DeviceListUpdateEvent += CBox_DeviceListUpdateEvent;
 
-            CrestronConsole.PrintLine($"Decoder Initialized");
+            CrestronConsole.PrintLine($"Decoder {_myDecoderId} Initialized");
         }
+
+
 
         private void CBox_DeviceListUpdateEvent(object sender, DeviceListUpdateEventArgs e)
         {
-            CrestronConsole.PrintLine($"Decoder has received a CBox event");
-            CrestronConsole.PrintLine($"CBox received a device list update.");
-            CrestronConsole.PrintLine($"Decoders: {e.decoderCount}");
-            CrestronConsole.PrintLine($"Encoders: {e.encoderCount}");
+            //CrestronConsole.PrintLine($"Decoder has received a CBox event");
+            //CrestronConsole.PrintLine($"CBox received a device list update.");
+            //CrestronConsole.PrintLine($"Decoders: {e.decoderCount}");
+            //CrestronConsole.PrintLine($"Encoders: {e.encoderCount}");
         }
 
         public void RequestVideoRoute(string switchType, ushort sourceIndex)
         {
-            CrestronConsole.PrintLine($"Decoder requested video switch!");
-
-
             try
             {
-                lock (CBox._listLock)
+                // Get the source ID from CBox's public list
+                if (sourceIndex > 0 && sourceIndex <= CBox.Instance.mxnetEncoders.Count)
                 {
-                    CBox.Instance.PrintLists();
+                    string sourceId = CBox.Instance.mxnetEncoders[sourceIndex - 1].id;
 
-                    // Get the source ID from CBox's public list
-                    if (sourceIndex > 0 && sourceIndex <= CBox.Instance.mxnetEncoders.Count)
-                    {
-                        string sourceId = CBox.Instance.mxnetEncoders[sourceIndex - 1].id;
-
-                        // Call the CBox singleton to send the command
-                        CBox.Instance.Switch(switchType, sourceId, _myDecoderId);
-                    }
-                    else
-                    {
-                        CrestronConsole.PrintLine($"MxnetEncoders count: {CBox.Instance.mxnetEncoders.Count}");
-                        CrestronConsole.PrintLine($"Invalid source index: {sourceIndex}");
-                    }
+                    // Call the CBox singleton to send the command
+                    CBox.Instance.Switch(switchType, sourceId, _myDecoderId);
+                }
+                else
+                {
+                    CrestronConsole.PrintLine($"MxnetEncoders count: {CBox.Instance.mxnetEncoders.Count}");
+                    CrestronConsole.PrintLine($"Invalid source index: {sourceIndex}");
                 }
             }
             catch (Exception e)
@@ -87,36 +72,30 @@ namespace TSI.MXNet
             }
         }
 
-        // --- C# Event Handlers (Listening to CBox) ---
 
-        private void CBox_RouteEvent(object sender, RouteEventArgs e)
+        private void CBox_RouteEvent(object sender, RouteEventArgs args)
         {
-            CrestronConsole.PrintLine($"Decoder recieved route event");
-            // ** This is the critical filter **
-            // Only process this event if it's meant for THIS decoder instance
-            if (e.DecoderId == _myDecoderId)
-            {
-                // 1. Set the S+ "getter" properties
-                CurrentSourceIndex = e.SourceIndex;
-                CurrentSourceId = e.SourceId;
-                IsStreamOn = e.StreamOn;
+            //CrestronConsole.PrintLine($"Decoder {_myDecoderId} recieved route event from CBox");
+            //CrestronConsole.PrintLine($"Route Event: {args.DecoderId} => {args.SourceId}");
 
-                // 2. Fire the S+ compatible event
-                CurrentRouteChanged?.Invoke(this, e);
+            if (args.DecoderId == _myDecoderId)
+            {
+                CurrentSourceIndex = args.SourceIndex;
+                CurrentSourceId = args.SourceId;
+                IsStreamOn = args.StreamOn;
+
+                CurrentRouteChanged?.Invoke(this, args);
             }
         }
 
-        private void CBox_ResponseErrorEvent(object sender, ResponseErrorEventArgs e)
+        private void CBox_ResponseErrorEvent(object sender, ResponseErrorEventArgs args)
         {
-            // Check if the error command was related to this decoder
-            if (e.Cmd.Contains(_myDecoderId))
+            if (args.Cmd.Contains(_myDecoderId))
             {
-                // 1. Set the S+ "getter" properties
-                LastError = e.Error;
-                LastErrorCmd = e.Cmd;
+                LastError = args.Error;
+                LastErrorCmd = args.Cmd;
 
-                // 2. Fire the S+ compatible event
-                ErrorReceived?.Invoke(this, e);
+                ErrorReceived?.Invoke(this, args);
             }
         }
 
