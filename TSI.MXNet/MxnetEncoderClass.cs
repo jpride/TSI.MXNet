63bfa65755fffa42065ddf1a93fa0d74df663253
Jeff Pride <jpride@gmail.com>
2025-11-07 16:14:24 -0600

    Removed Methods not necessary

--- TSI.MXNet/MxnetEncoderClass.cs
+++ TSI.MXNet/MxnetEncoderClass.cs
@@ -34,66 +34,6 @@ namespace TSI.MXNet
 
         }
 
-        public void RequestVideoRoute(string switchType, ushort sourceIndex)
-        {
-            try
-            {
-                // Get the source ID from CBox's public list
-                if (sourceIndex > 0 && sourceIndex <= CBox.Instance.mxnetEncoders.Count)
-                {
-                    string sourceId = CBox.Instance.mxnetEncoders[sourceIndex - 1].id;
-
-                    // Call the CBox singleton to send the command
-                    CBox.Instance.Switch(switchType, sourceId, _myEncoderId);
-                }
-                else //if sourceindex is 0 perhaps send videopathdisable?)
-                {
-                    DebugUtility.DebugPrint(CBox.Instance.Debug == 1, $"MxnetEncoders count: {CBox.Instance.mxnetEncoders.Count}");
-                    DebugUtility.DebugPrint(CBox.Instance.Debug == 1, $"Invalid source index: {sourceIndex}");
-                }
-            }
-            catch (Exception e)
-            {
-                CrestronConsole.PrintLine($"Error in RequestVideoRoute: {e.Message}");
-            }
-        }
-
-        public void RequestVideoPathDisable()
-        {
-            try
-            {
-                CBox.Instance.VideoPathDisable(_myEncoderId);
-            }
-            catch (Exception e)
-            {
-                CrestronConsole.PrintLine($"Error in RequestVideoPathDisable: {e.Message}");
-            }
-        }
-
-        public void RequestRs232CommandSend(string command, string HexOrAscii)
-        {
-            try
-            {
-                CBox.Instance.SendRs232Command(_myEncoderId, command, HexOrAscii);
-            }
-            catch (Exception e)
-            {
-                CrestronConsole.PrintLine($"Error in RequestRs232CommandSend: {e.Message}");
-            }
-        }
-
-        public void RequestStreamStatusChange(ushort OnOrOff)
-        {
-            try
-            {
-                CBox.Instance.SetStreamStatus(_myEncoderId, OnOrOff);
-            }
-            catch (Exception e)
-            {
-                CrestronConsole.PrintLine($"Error in RequestStreamOn: {e.Message}");
-            }
-        }
-
         public void Dispose()
         {
             // Unsubscribe from the CBox events to prevent memory leaks
